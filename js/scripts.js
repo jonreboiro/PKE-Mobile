af543d5170e6fbddab1ff2853a240cfb85a7430f
jonreboiro <jreboiro001@ikasle.ehu.eus>
2024-11-25 19:35:17 +0100

    version 1.2

--- js/scripts.js
+++ js/scripts.js
@@ -105,3 +105,23 @@ document.getElementById("nextMonth").addEventListener("click", () => {
 
 // Initial Render
 renderCalendar(currentDate);
+
+        // Open the sidebar
+        toggleButton.addEventListener('click', () => {
+            sidebar.classList.add('show');
+            content.classList.add('shifted');
+        });
+
+        // Close the sidebar
+        closeButton.addEventListener('click', () => {
+            sidebar.classList.remove('show');
+            content.classList.remove('shifted');
+        });
+
+        // Close the sidebar when clicking outside of it
+        document.addEventListener('click', (event) => {
+            if (!sidebar.contains(event.target) && !toggleButton.contains(event.target)) {
+                sidebar.classList.remove('show');
+                content.classList.remove('shifted');
+            }
+        });
